f7402f7bc6bb6a324e8c35b0d45f27ea7efb98d6
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-11 15:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let i=0;i<sampleObject.length; i++){
+		if(sampleObject.i == key){
+			console.log("true");
+		}else {
+			console.log("false");
+		}
+	}
 }
 
 // Do not change the code below
